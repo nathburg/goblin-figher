4952a98782f71811835d7fe64aa416d431475b41
Nathan Burgess <nathans.email.address@gmail.com>
2022-08-04 16:45:04 -0700

    attacking goblins works

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const goblinContainerEl = document.getElementById("goblin-container");
-const goblinNameInputEl = document.getElementById("goblin-name-input");
+const goblinContainerEl = document.getElementById('goblin-container');
+const goblinNameInputEl = document.getElementById('goblin-name-input');
 const goblinNameButtonEl = document.getElementById("goblin-name-button");
+const yourStatusEl = document.getElementById("your-status");
+const goblinStatusEl = document.getElementById("goblin-status");
+const protagonistStatsEl = document.getElementById("protagonist-stats");
 
+let yourHP = 10;
 let goblins = [
     {
         name: "Bad Goblin",
@@ -16,7 +20,7 @@ let goblins = [
 displayGoblins();
 
 goblinNameButtonEl.addEventListener('click', () => {
-    if (goblinNameInputEl.value != "") {
+    if (goblinNameInputEl.value !== '') {
         const newGoblin = {
             name: goblinNameInputEl.value,
             HP: 3
@@ -24,13 +28,32 @@ goblinNameButtonEl.addEventListener('click', () => {
         goblins.push(newGoblin);
         displayGoblins();
     }
+    goblinNameInputEl.value = '';
 });
 
 function renderGoblin(goblin) {
-    const goblinEl = document.createElement('div');
+    const goblinEl = document.createElement('button');
     goblinEl.textContent = `${goblin.name} 👹 ${goblin.HP} HP`;
     goblinEl.classList.add('goblin');
-
+    goblinEl.addEventListener('click', () => {
+        if (goblin.HP > 0) {
+            if (Math.random() > .5) {
+                goblin.HP--;
+                goblinStatusEl.textContent = `You hit ${goblin.name}!`;
+                displayGoblins();
+                // if (goblin.HP === 0) {
+            } else {
+                goblinStatusEl.textContent = `Your attack missed!`;
+            }
+            if (Math.random() > .7) {
+                yourHP--;
+                yourStatusEl.textContent = `You've been hit by ${goblin.name}!`;
+                protagonistStatsEl.textContent = `${yourHP} HP`;
+            } else {
+                yourStatusEl.textContent = `${goblin.name}'s attack missed!`;
+            }
+        }
+    });    
     return goblinEl;
 }
 
@@ -41,3 +64,5 @@ function displayGoblins() {
         goblinContainerEl.append(newGoblin);
     }
 }
+
+
